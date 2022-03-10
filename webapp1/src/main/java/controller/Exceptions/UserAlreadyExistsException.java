e5e6f4e538af210c2d6bcdba1fb7ca80026d8573
OliverAndrango <oliver.uriel.andrango@estudiantat.upc.edu>
2022-03-10 02:44:13 +0100

    Mensaje Error v2

--- webapp1/src/main/java/controller/Exceptions/UserAlreadyExistsException.java
+++ webapp1/src/main/java/controller/Exceptions/UserAlreadyExistsException.java
@@ -3,6 +3,6 @@ package controller.Exceptions;
 public class UserAlreadyExistsException extends Exception {
 
     public UserAlreadyExistsException() {
-        super("The user you want to add already exists!");
+        super("The user already exists!");
     }
 }
