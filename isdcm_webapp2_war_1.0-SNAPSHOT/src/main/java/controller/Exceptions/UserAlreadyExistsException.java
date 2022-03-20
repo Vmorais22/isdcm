24c17ca7118da8df601d23412dd57dd2d6dcb87e
UrielAndrango <61828847+UrielAndrango@users.noreply.github.com>
2022-03-20 13:57:56 +0100

    Delete isdcm_webapp2_war_1.0-SNAPSHOT directory

--- isdcm_webapp2_war_1.0-SNAPSHOT/src/main/java/controller/Exceptions/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package controller.Exceptions;
-
-public class UserAlreadyExistsException extends Exception {
-
-    public UserAlreadyExistsException() {
-        super("The user already exists!");
-    }
-}
--- isdcm_webapp2_war_1.0-SNAPSHOT/src/main/java/controller/Exceptions/UserDontExistsException.java
+++
@@ -1,8 +0,0 @@
-package controller.Exceptions;
-
-public class UserDontExistsException extends Exception {
-
-    public UserDontExistsException() {
-        super("The user don't exists!");
-    }
-}
